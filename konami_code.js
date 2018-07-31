c8d988b97738e30bce4a11477216e332a18a77cf
Jonathan Hosein <jonathan.hosein@live.com>
2018-07-31 03:44:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,21 +15,23 @@ function init() {
   // your code here
 document.body.addEventListener('keydown', keydownHandler(event));
   let index = 0;
-}
 
-function keydownHandler(e) {
-  let key = e.key;
-  console.log(key);
 
-  if(e.key === codes[index]) {
-    index++;
-    if(index === codes.length)
-    {
-      alert("Hurray!");
+  function keydownHandler(e) {
+    let key = e.key;
+    console.log(key);
+
+    if(e.key === codes[index]) {
+      index++;
+      if(index === codes.length)
+      {
+        alert("Hurray!");
+        index = 0;
+      }
+    }
+    else {
       index = 0;
     }
   }
-  else {
-    index = 0;
-  }
+
 }
