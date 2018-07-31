b6ead5ba8c916d79f32136ddec2e24f4a8902484
Jonathan Hosein <jonathan.hosein@live.com>
2018-07-31 03:56:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -22,7 +22,7 @@ function init() {
 
     if(event.key === codes[index]) {
       index++;
-      if(index === (codes.length)
+      if(index === codes.length)
       {
         alert("Hurray!");
         index = 0;
